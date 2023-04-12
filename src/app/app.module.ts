fab5ad32ce27979ccc01c23adff1b341324006e8
Keven Barauna <keven.barauna@solutis.com.br>
2023-04-11 21:58:02 -0300

    add structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/components/header/header.component';
+import { HeaderModule } from './core/components/header/header.module';
+import { SideMenuModule } from './core/components/side-menu/side-menu.module';
+import { ContentPageModule } from './shared/components/content-page/content-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    SideMenuModule,
+    ContentPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/content-page/content-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-content-page',
+  templateUrl: './content-page.component.html',
+  styleUrls: ['./content-page.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class ContentPageComponent {
+
+}
